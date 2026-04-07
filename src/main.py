d95755dbdc83a84ea35d5ec7841ffeba822b6d82
IamnotWise <akeemlarkins@gmail.com>
2026-04-06 21:00:51 -0400

    First commit for site gen

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from textnode import TextNode, TextType
+
+
+def main():
+    print("hello world")
+    node = TextNode("Need a Latina",TextType.links, "https://www.boot.dev")
+    print(node)
+if __name__ == '__main__':
+    main()
