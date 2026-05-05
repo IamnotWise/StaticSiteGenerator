fc240ac388702813393a61142537c65cbde9e918
IamnotWise <akeemlarkins@gmail.com>
2026-05-05 18:10:51 -0400

    static_copier

--- src/main.py
+++ src/main.py
@@ -1,9 +1,17 @@
 from textnode import TextNode, TextType
+import os, shutil
 
-
+def copy_static():
+    if os.path.exists('public'):
+        shutil.rmtree('public')
+    shutil.copytree('static', 'public')
+       
 def main():
-    print("hello world")
-    node = TextNode("Need a Latina",TextType.links, "https://www.boot.dev")
-    print(node)
+    # This is just a test to make sure the TextNode class works
+    #print("hello world")
+    #node = TextNode("Need a Latina",TextType.links, "https://www.boot.dev")
+    #print(node)
+    copy_static()
+
 if __name__ == '__main__':
     main()
